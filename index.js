520542bea736c329a066938ce3b29c6d8d34a60e
Ola Ouso <ouso.ola@gmail.com>
2020-10-19 16:54:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(outerParam = "*") {
+    return function(param = "special") {
+        return `You are ${outerParam}${param}${outerParam}!`
+    }
+}
+
+const Calculator = {
+    add : () => 1 + 3,
+    subtract : () => 1 - 3,
+    multiply : () => 1 * 3,
+    divide : () => 10 / 5
+}
+
+ function actionApplyer(startingInt, arrayOfFunctions) {
+     for (const fn of arrayOfFunctions) {
+        startingInt = fn(startingInt)
+     }
+     return startingInt
+ } 
